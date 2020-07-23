efe863a2e12fa01e8c83d1bcd995cad19962abcd
leoncordell <46611965+leoncordell@users.noreply.github.com>
2020-07-23 15:10:07 +0100

    Original commits

---
+++ src/main/java/com/clocks/speakingclock/DisplayTime.java
@@ -0,0 +1,60 @@
+package com.clocks.speakingclock;
+
+public class DisplayTime {
+
+    private static String[] hours = {"one","two","three","four","five","six","seven","eight","nine","ten","eleven",
+    "twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen",
+            "nineteen","twenty","twenty one","twenty two","twenty three","twenty four"};
+
+    private static String[] units = {"one","two","three","four","five","six","seven","eight","nine","ten"};
+
+    private static String[] teens = {"Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen",
+        "Nineteen"};
+    private static String[] tens = {"","twenty","thirty","forty","fifty"};
+
+    public String getTime(String inputStr){
+        if(inputStr==null || !inputStr.contains(":")){
+            return "Please enter a valid  time";
+        }
+        String[] input = inputStr.split(":");
+
+        int hour = Integer.parseInt(input[0]);
+        int min = Integer.parseInt(input[1]);
+
+        if(hour==12 && min==0){
+            return "It's Midday";
+        }
+        if(hour==24 && min==0){
+            return "It's Midnight";
+        }
+
+        String  result = "It's " + hours[hour -1] + " ";
+        if(min==0){
+            result +=   "o'clock";
+        }else{
+            result += getMinuteText(min);
+        }
+
+
+        return result;
+    }
+
+    private String getMinuteText(int min){
+        String result ="";
+        if(min < 11){
+            result=units[min-1];
+        }else if(min < 20){
+            result = teens[min%10 -1].toLowerCase();
+        }else{
+            result = tens[min/10 -1] ;
+            if(min%10 >0) {
+              result += " " + units[min % 10 - 1];
+            }
+            result = result;
+        }
+
+        return result;
+    }
+
+
+}
---
+++ test/src/test/java/com/clocks/speakingclock/DisplayTimeTest.java
@@ -0,0 +1,83 @@
+package com.clocks.speakingclock;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DisplayTimeTest {
+
+    @Test
+    public void getTimeTest(){
+        String input1 ="";
+        DisplayTime displayTime = new DisplayTime();
+        assertEquals("Please enter a valid  time",displayTime.getTime(input1));
+    }
+
+    @Test
+    public void getTimeHour(){
+        String input1 ="08:00";
+        DisplayTime displayTime = new DisplayTime();
+        String result = displayTime.getTime(input1);
+        assertEquals("It's eight o'clock",result);
+    }
+
+
+
+    @Test
+    public void getTimeHourMidday(){
+        String input1 ="12:00";
+        DisplayTime displayTime = new DisplayTime();
+        String result = displayTime.getTime(input1);
+        assertEquals("It's Midday",result);
+    }
+
+    @Test
+    public void getTimeHourMidnight(){
+        String input1 ="24:00";
+        DisplayTime displayTime = new DisplayTime();
+        String result = displayTime.getTime(input1);
+        assertEquals("It's Midnight",result);
+    }
+
+
+    @Test
+    public void getTimeMinSingle(){
+        String input1 ="08:05";
+        DisplayTime displayTime = new DisplayTime();
+        String result = displayTime.getTime(input1);
+        assertEquals("It's eight five",result);
+    }
+
+    @Test
+    public void getTimeMinTeen(){
+        String input1 ="08:12";
+        DisplayTime displayTime = new DisplayTime();
+        String result = displayTime.getTime(input1);
+        assertEquals("It's eight twelve",result);
+    }
+
+
+    @Test
+    public void getTimeMinTens(){
+        String input1 ="08:40";
+        DisplayTime displayTime = new DisplayTime();
+        String result = displayTime.getTime(input1);
+        assertEquals("It's eight forty",result);
+    }
+    @Test
+    public void getTimeMinTensUnits(){
+        String input1 ="08:47";
+        DisplayTime displayTime = new DisplayTime();
+        String result = displayTime.getTime(input1);
+        assertEquals("It's eight forty seven",result);
+    }
+
+
+    @Test
+    public void getTimeMinTensUnits2(){
+        String input1 ="08:59";
+        DisplayTime displayTime = new DisplayTime();
+        String result = displayTime.getTime(input1);
+        assertEquals("It's eight fifty nine",result);
+    }
+}
